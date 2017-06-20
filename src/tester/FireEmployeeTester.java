4f468d035b584a0d2db0346e586a791bb7de6743
Mirhawk <piyushphalak@yahoo.co.in>
2017-06-20 23:50:34 +0530

    Added functionality of Fire employee.

--- src/tester/FireEmployeeTester.java
+++ src/tester/FireEmployeeTester.java
@@ -1,5 +1,35 @@
 package tester;
 
-public class FireEmployeeTester {
+import static utils.HibernateUtils.getSf;
 
+import java.util.Scanner;
+
+import org.hibernate.HibernateException;
+
+import dao.DepartmentDAO;
+import pojo.DepartmentPOJO;
+import pojo.EmployeePOJO;
+
+public class FireEmployeeTester {
+	public static void main(String[] args) {
+		getSf();
+		DepartmentDAO departmentDAO;
+		DepartmentPOJO department;
+		try(Scanner sc = new Scanner(System.in)) {
+			departmentDAO = new DepartmentDAO();
+//			System.out.println("Enter the Department Number : ");
+//			int deptid = sc.nextInt();
+		
+
+			System.out.print("Enter empID to fire: ");
+			
+			departmentDAO.fireEmployee(sc.nextInt());
+		}
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		finally {
+			getSf().close();
+		}
+	}
 }
