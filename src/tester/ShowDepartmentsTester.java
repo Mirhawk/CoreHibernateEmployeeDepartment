66d8d8e0fd5650b300afdf4866889ea3599e93bf
Mirhawk <piyushphalak@yahoo.co.in>
2017-06-20 23:50:50 +0530

    Added functionality of Show Department details.

--- src/tester/ShowDepartmentsTester.java
+++ src/tester/ShowDepartmentsTester.java
@@ -1,5 +1,28 @@
 package tester;
 
-public class ShowDepartmentsTester {
+import static utils.HibernateUtils.getSf;
 
+import java.util.Scanner;
+
+import org.hibernate.HibernateException;
+
+import dao.DepartmentDAO;
+
+public class ShowDepartmentsTester {
+	public static void main(String[] args) {
+		getSf();
+		DepartmentDAO departmentDAO;
+	
+		try(Scanner sc = new Scanner(System.in)) {
+			departmentDAO = new DepartmentDAO();
+			System.out.print("Enter departemntID to show details of: ");
+			System.out.println(departmentDAO.getDepartment(sc.nextInt()));
+		}
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		finally {
+			getSf().close();
+		}
+	}
 }
